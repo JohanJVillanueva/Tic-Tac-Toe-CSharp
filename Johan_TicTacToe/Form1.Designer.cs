94ae32e6f1e42161baec457ea53a02b178309790
JohanJVillanueva <johanjvillanueva@iskolarngbayan.pup.edu.ph>
2022-05-23 20:17:40 +0800

    Implementing design I made in photoshop, learning how to add icons on buttons as well. I did not update the repo in a week because of other activities

--- Johan_TicTacToe/Form1.Designer.cs
+++ Johan_TicTacToe/Form1.Designer.cs
@@ -28,13 +28,42 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btnRetry = new System.Windows.Forms.Button();
+            this.btnMenu = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // btnRetry
+            // 
+            this.btnRetry.BackColor = System.Drawing.Color.RoyalBlue;
+            this.btnRetry.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnRetry.BackgroundImage")));
+            this.btnRetry.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnRetry.Location = new System.Drawing.Point(22, 23);
+            this.btnRetry.Name = "btnRetry";
+            this.btnRetry.Size = new System.Drawing.Size(209, 202);
+            this.btnRetry.TabIndex = 0;
+            this.btnRetry.Text = "button1";
+            this.btnRetry.UseVisualStyleBackColor = false;
+            this.btnRetry.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnMenu
+            // 
+            this.btnMenu.BackColor = System.Drawing.Color.DarkOrange;
+            this.btnMenu.Location = new System.Drawing.Point(441, 38);
+            this.btnMenu.Name = "btnMenu";
+            this.btnMenu.Size = new System.Drawing.Size(111, 53);
+            this.btnMenu.TabIndex = 1;
+            this.btnMenu.Text = "button2";
+            this.btnMenu.UseVisualStyleBackColor = false;
+            this.btnMenu.Click += new System.EventHandler(this.button2_Click);
             // 
             // Form1
             // 
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Inherit;
-            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.BackColor = System.Drawing.Color.Wheat;
             this.ClientSize = new System.Drawing.Size(599, 598);
+            this.Controls.Add(this.btnMenu);
+            this.Controls.Add(this.btnRetry);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.MaximizeBox = false;
             this.MinimizeBox = false;
@@ -47,5 +76,8 @@
         }
 
         #endregion
+
+        private Button btnRetry;
+        private Button btnMenu;
     }
 }
