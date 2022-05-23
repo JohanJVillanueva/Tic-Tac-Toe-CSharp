dc868ae9350fd9b5fe414982f954eb4a88a50c46
JohanJVillanueva <johanjvillanueva@iskolarngbayan.pup.edu.ph>
2022-05-23 20:41:38 +0800

    Added Retry Icon, TicTacToe Logo, and Hamburger Menu Buttons on top, also added bg image on where the X, and O icons will be placed

--- Johan_TicTacToe/Form1.Designer.cs
+++ Johan_TicTacToe/Form1.Designer.cs
@@ -31,6 +31,10 @@
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.btnRetry = new System.Windows.Forms.Button();
             this.btnMenu = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             this.SuspendLayout();
             // 
             // btnRetry
@@ -38,30 +42,56 @@
             this.btnRetry.BackColor = System.Drawing.Color.RoyalBlue;
             this.btnRetry.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnRetry.BackgroundImage")));
             this.btnRetry.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.btnRetry.Location = new System.Drawing.Point(22, 23);
+            this.btnRetry.Location = new System.Drawing.Point(34, 23);
             this.btnRetry.Name = "btnRetry";
-            this.btnRetry.Size = new System.Drawing.Size(209, 202);
+            this.btnRetry.Size = new System.Drawing.Size(103, 98);
             this.btnRetry.TabIndex = 0;
-            this.btnRetry.Text = "button1";
             this.btnRetry.UseVisualStyleBackColor = false;
             this.btnRetry.Click += new System.EventHandler(this.button1_Click);
             // 
             // btnMenu
             // 
             this.btnMenu.BackColor = System.Drawing.Color.DarkOrange;
-            this.btnMenu.Location = new System.Drawing.Point(441, 38);
+            this.btnMenu.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnMenu.BackgroundImage")));
+            this.btnMenu.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.btnMenu.Location = new System.Drawing.Point(453, 25);
             this.btnMenu.Name = "btnMenu";
-            this.btnMenu.Size = new System.Drawing.Size(111, 53);
+            this.btnMenu.Size = new System.Drawing.Size(103, 97);
             this.btnMenu.TabIndex = 1;
-            this.btnMenu.Text = "button2";
+            this.btnMenu.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
             this.btnMenu.UseVisualStyleBackColor = false;
             this.btnMenu.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.InitialImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.InitialImage")));
+            this.pictureBox1.Location = new System.Drawing.Point(193, 23);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(201, 99);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(35, 136);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(522, 432);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 3;
+            this.pictureBox2.TabStop = false;
             // 
             // Form1
             // 
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Inherit;
-            this.BackColor = System.Drawing.Color.Wheat;
-            this.ClientSize = new System.Drawing.Size(599, 598);
+            this.BackColor = System.Drawing.Color.PeachPuff;
+            this.ClientSize = new System.Drawing.Size(590, 598);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.btnMenu);
             this.Controls.Add(this.btnRetry);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
@@ -71,6 +101,8 @@
             this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
             this.Text = "Johan\'s TicTacToe";
             this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -79,5 +111,7 @@
 
         private Button btnRetry;
         private Button btnMenu;
+        private PictureBox pictureBox1;
+        private PictureBox pictureBox2;
     }
 }
--- Johan_TicTacToe/Form1.cs
+++ Johan_TicTacToe/Form1.cs
@@ -21,5 +21,10 @@ namespace Johan_TicTacToe
         {
 
         }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
