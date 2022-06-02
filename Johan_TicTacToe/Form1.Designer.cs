828d37a8c5b61e3fd3aadfa53636359804cf5c38
JohanJVillanueva <johanjvillanueva@iskolarngbayan.pup.edu.ph>
2022-06-02 21:55:11 +0800

    Centered the windows form and added icon.ico as taskbar icon

--- Johan_TicTacToe/Form1.Designer.cs
+++ Johan_TicTacToe/Form1.Designer.cs
@@ -444,10 +444,12 @@
             this.Controls.Add(this.btnMenu);
             this.Controls.Add(this.btnRetry);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "Form1";
             this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Johan\'s TicTacToe";
             this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
